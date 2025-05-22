c9cd35e0a4e716493d0361bdd9dd677b1675fb87
darius-it <179517889+darius-it@users.noreply.github.com>
2025-05-22 23:51:30 +0200

    feat: add opening and closing windows

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="p-4 flex flex-col gap-4">
-    <DesktopWindow >
+    <DesktopWindow 
+      name="Thunderbird" 
+      :lastClickedWindow="currentlyOpenWindows.lastClickedWindow"
+      :currentlyOpenWindows="currentlyOpenWindows.openWindows"
+      @clickInsideWindow="setLastClickedWindow" 
+      @close="closeWindow"
+    >
       hallo
     </DesktopWindow>
-    <DesktopWindow />
+
+    <DesktopWindow 
+      name="Browser" 
+      :lastClickedWindow="currentlyOpenWindows.lastClickedWindow"
+      :currentlyOpenWindows="currentlyOpenWindows.openWindows"
+      @clickInsideWindow="setLastClickedWindow" 
+      @close="closeWindow"
+    />
 
     <div id="row-1" class="flex flex-row items-center gap-6">
       <DesktopIcon
-        windowName="Start"
+        windowName="Thunderbird"
         @openWindow="(windowName) => openWindow(windowName)"
       >
         <img src="/icons/folder.png" alt="Start Icon" class="w-16 h-16" />
       </DesktopIcon>
       <DesktopIcon
-        windowName="Start"
+        windowName="Browser"
         @openWindow="(windowName) => openWindow(windowName)"
       >
         <img src="/icons/folder.png" alt="Start Icon" class="w-16 h-16" />
@@ -62,8 +75,24 @@
 </template>
 
 <script lang="ts" setup>
+const currentlyOpenWindows = useOpenWindowsStore();
+
 const openWindow = (windowName: string) => {
   console.log(`Opening window: ${windowName}`);
-  // Logic to open the window goes here
+  currentlyOpenWindows.openWindows.push(windowName);
+};
+
+const closeWindow = (windowName: string) => {
+  console.log(`Closing window: ${windowName}`);
+  const index = currentlyOpenWindows.openWindows.indexOf(windowName);
+  if (index > -1) {
+    currentlyOpenWindows.openWindows.splice(index, 1);
+  }
+};
+
+const setLastClickedWindow = (windowName: string) => {
+  console.log(`Last clicked window: ${windowName}`);
+  // Logic to set the last clicked window goes here
+  currentlyOpenWindows.lastClickedWindow = windowName;
 };
 </script>
