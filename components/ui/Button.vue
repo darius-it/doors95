20e0c1bcf8474dca3babff1950fde9c17d0879cd
darius-it <179517889+darius-it@users.noreply.github.com>
2025-05-23 13:36:30 +0200

    styles: add cursor pointer to button

--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="font-[400] text-[0.6rem] text-black bg-primary-button transition-all duration-200 ease-in-out tracking-wide px-6 py-1.5 border-2 border-t-white border-l-white border-r-[#858585] border-b-[#858585] border-[#a0a0a0] inline-block focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#0A0A0A]"
+    class="font-[400] text-[0.6rem] text-black bg-primary-button transition-all duration-200 ease-in-out tracking-wide px-6 py-1.5 border-2 border-t-white border-l-white border-r-[#858585] border-b-[#858585] border-[#a0a0a0] inline-block focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#0A0A0A] cursor-pointer"
     @click="$emit('click', $event)"
   >
     <slot />
