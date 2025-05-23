74ee0761f0723716f359518e8b74bbbd3fb23bfd
Joniprog <jonathanzwierlein@gmail.com>
2025-05-23 17:27:58 +0200

    minecraft addiction

---
+++ components/apps/MinecraftGame.vue
@@ -0,0 +1,3 @@
+<template>
+  <iframe src="https://classic.minecraft.net/" frameborder="0" scrolling="no" width="1500px" height="700px" loading="lazy"></iframe>
+</template>
---
+++ components/icon/Minecraft.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="/images/minecraft.webp" width="56px" height="56px">
+</template>
