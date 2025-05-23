bf478d346eed2471ac7d021b089ce68124792a9f
darius-it <179517889+darius-it@users.noreply.github.com>
2025-05-23 22:35:47 +0200

    fix: reactivity of bottom bar smiley

--- components/layout/BottomBarSmiley.vue
+++ components/layout/BottomBarSmiley.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="text-[1.3rem] tracking-wider">
-    <img :src=source alt="Smiley" width="24px" height="24px">
+    <img :src=source alt="Smiley" width="24px" height="24px" class="rounded-full">
   </div>
 </template>
 
 <script lang="ts" setup>
 
-const infectionLevel = useMissionsStore().infectionLevel
+const { infectionLevel } = storeToRefs(useMissionsStore())
 
 const source = computed(() => {
-  if (infectionLevel < 25) return '/images/green_smiley.png'
-  if (infectionLevel < 50) return '/images/yellow_smiley.png'
-  if (infectionLevel < 75) return '/images/red_smiley.png'
+  if (infectionLevel.value < 25) return '/images/green_smiley.png'
+  if (infectionLevel.value < 50) return '/images/yellow_smiley.png'
+  if (infectionLevel.value < 75) return '/images/red_smiley.png'
   else return '/images/cursed_smiley.jpg'
 })
 </script>
