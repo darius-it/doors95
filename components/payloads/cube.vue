686895c4c4ffdda5c154fa6354e5309e6308dd75
Joniprog <jonathanzwierlein@gmail.com>
2025-05-23 22:07:48 +0200

    cube

--- components/payloads/cube.vue
+++ components/payloads/cube.vue
@@ -11,7 +11,7 @@ const cubeContainer = ref(null)
 onMounted(() => {
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  const renderer = new THREE.WebGLRenderer({ antialias: true,alpha:true})
   renderer.setSize(window.innerWidth, window.innerHeight)
   cubeContainer.value.appendChild(renderer.domElement)
 
