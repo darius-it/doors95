2cc2259ec485f6214bf6d039756f5935108d2186
darius-it <179517889+darius-it@users.noreply.github.com>
2025-05-23 08:57:26 +0200

    feat: add methods to window store for modifying windows

--- stores/windows.ts
+++ stores/windows.ts
@@ -5,5 +5,23 @@ export const useOpenWindowsStore = defineStore("windows", {
     openWindows: [] as string[],
     lastClickedWindow: "",
   }),
-  actions: {}
+  actions: {
+    openWindow(windowName: string) {
+      if (!this.openWindows.includes(windowName)) {
+        this.openWindows.push(windowName)
+      }
+    },
+    closeWindow(windowName: string) {
+      const index = this.openWindows.indexOf(windowName)
+      if (index !== -1) {
+        this.openWindows.splice(index, 1)
+      }
+    },
+    closeAllWindows() {
+      this.openWindows = []
+    },
+    setLastClickedWindow(windowName: string) {
+      this.lastClickedWindow = windowName
+    }
+  }
 })
